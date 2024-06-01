36137419d35bf342af6a8e994affa74bd41ef835
Zhang Zhuyue <rjys365@hotmail.com>
2024-06-01 15:16:52 +0800

    bugfix: 删除kubelet中多余的cancel，否则会直接退出

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -44,7 +44,7 @@ func NewKubelet(node config.Node) *Kubelet {
 
 func (k *Kubelet) Run(ctx context.Context, cancel context.CancelFunc) error {
 	//cli, _ := cri.GetClient()
-	defer cancel()
+	// defer cancel()
 	k.podListWatcher = listwatch.NewListWatchFromClient(k.podClient)
 	go func() {
 		defer cancel()
